59bb02123bba83b90302ab6a642e52f24886bbff
David Duarte <david.duarte@tecnico.ulisboa.pt>
2016-02-05 20:15:27 +0000

    Add first test
    - Add Vagrantfile
    - Update gitignore
    - Remove IDEA files
    - Update application.conf
    
    

---
+++ ldap/src/test/scala/pt/tecnico/dsi/ldap/ConnectionSpec.scala
@@ -0,0 +1,8 @@
+package pt.tecnico.dsi.ldap
+
+/**
+  * Created by david on 2/5/16.
+  */
+class ConnectionSpec {
+
+}
---
+++ ldap/src/test/scala/pt/tecnico/dsi/ldap/UnitSpec.scala
@@ -0,0 +1,5 @@
+package pt.tecnico.dsi.ldap
+
+import org.scalatest.{FlatSpec, Matchers}
+
+abstract class UnitSpec extends FlatSpec with Matchers
