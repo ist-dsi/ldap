c39fc5dbc7f5ec5a9c62f59569e3b68d69949e23
David Duarte <david.duarte@tecnico.ulisboa.pt>
2016-04-05 16:40:37 +0100

    Convert LdativeAttribute to native scala

--- ldap/src/main/scala/pt/tecnico/dsi/ldap/Ldap.scala
+++ ldap/src/main/scala/pt/tecnico/dsi/ldap/Ldap.scala
@@ -1,65 +1,76 @@
 package pt.tecnico.dsi.ldap
 
-import java.security.KeyStore
-
 import com.typesafe.scalalogging.LazyLogging
-
 import org.ldaptive.pool._
-import org.ldaptive.provider.unboundid.UnboundIDProvider
-import org.ldaptive.ssl.{SslConfig, KeyStoreCredentialConfig}
 import org.ldaptive._
 
-class Ldap(settings: LdapSettings) extends LazyLogging {
+import scala.concurrent.Future
 
-  private val credential: Credential = new Credential(settings.bindPassword)
+class Ldap(val settings: Settings = new Settings()) extends LazyLogging {
 
-  private val connectionConfig = new ConnectionConfig(settings.host)
-  connectionConfig.setConnectTimeout(settings.connectionTimeout)
-  connectionConfig.setResponseTimeout(settings.responseTimeout)
-  connectionConfig.setUseStartTLS(false)
-  connectionConfig.setUseSSL(settings.enableSSL)
-  connectionConfig.setConnectionInitializer(new BindConnectionInitializer(settings.bindDN, credential))
+  import settings._
 
-  if (settings.enableSSL) {
-    val keyStoreConfig = new KeyStoreCredentialConfig()
-    keyStoreConfig.setTrustStore(settings.trustStore)
-    keyStoreConfig.setTrustStorePassword(settings.trustStorePassword)
-    keyStoreConfig.setTrustStoreType(KeyStore.getDefaultType)
-
-    val sslConfig = new SslConfig()
-    sslConfig.setCredentialConfig(keyStoreConfig)
-    sslConfig.setEnabledProtocols(settings.protocol)
-    sslConfig.setEnabledCipherSuites(settings.enabledAlgorithms)
-
-    connectionConfig.setSslConfig(sslConfig)
-  }
-
-  private val connectionFactory: DefaultConnectionFactory = new DefaultConnectionFactory(connectionConfig,
-    new UnboundIDProvider())
-
-  private val poolConfig: PoolConfig = new PoolConfig()
-  poolConfig.setMinPoolSize(settings.minPoolSize)
-  poolConfig.setMaxPoolSize(settings.maxPoolSize)
-  poolConfig.setValidatePeriodically(true)
-  poolConfig.setValidatePeriod(settings.validationPeriod)
-
-  private val pool: AbstractConnectionPool = new SoftLimitConnectionPool()
-  pool.setConnectionFactory(connectionFactory)
-  pool.setFailFastInitialize(true)
-  //Before connections are checked back into the pool a bind request will be made.
-  //This makes connections consistent with ConnectionInitializer from ConnectionConfig
-  pool.setPassivator(new BindPassivator(new BindRequest(settings.bindDN, credential)))
-  pool.setPoolConfig(poolConfig)
-  //SearchValidator - connection is valid if the search operation returns one or more results.
-  //Connections that fail validation are evicted from the pool.
-  pool.setValidator(new SearchValidator())
-  //Prunes connections from the pool based on how long they have been idle.
-  pool.setPruneStrategy(new IdlePruneStrategy(settings.prunePeriod, settings.pruneIdleTime))
-
-  implicit private val pooledConnectionFactory: PooledConnectionFactory = new PooledConnectionFactory(pool)
-
-  def initializePool(): Unit = {
+  private val connectionFactory: ConnectionFactory = if (enablePool) {
     pool.initialize()
     logger.info("Connection pool initialized successfully.")
+    pooledConnectionFactory
+  } else {
+    defaultConnectionFactory
   }
+
+  def close(): Unit = connectionFactory match {
+    case cf: PooledConnectionFactory =>
+      pool.close()
+    case _ => //Nothing to do
+  }
+
+  private def withConnection[R](f: Connection => R): Future[R] = Future {
+    connectionFactory.getConnection
+  }.flatMap { connection =>
+    try {
+      if (!connection.isOpen) {
+        connection.open()
+      }
+
+      Future(f(connection))
+    } catch {
+      case ex: LdapException | IllegalStateException => Future.failed(ex)
+    } finally {
+      connection.close()
+    }
+  }
+
+  def search(ou: String, filter: String, attributes: String*): Future[SearchResult] = withConnection { connection =>
+    val request = new SearchRequest(s"$ou,$baseDomain", filter, attributes: _*)
+    request.setDerefAliases(DerefAliases.valueOf(searchDereferenceAlias))
+    request.setSearchScope(SearchScope.valueOf(searchScope))
+    request.setSizeLimit(searchSizeLimit)
+    request.setTimeLimit(searchTimeLimit)
+
+    new SearchOperation(connection).execute(request).getResult
+  }
+
+  private def fixLdapEntry(entry: LdapEntry): Option[Entry] = {
+    import scala.collection.JavaConverters._
+
+    Option(entry)
+      .filter(_.getAttributes != null)
+      .map { e =>
+        val attributes = e.getAttributes.asScala.map { attribute =>
+          val values = if (attribute.isBinary) {
+            attribute.getBinaryValues.asScala.map { value =>
+              Binary(value)
+            }.toSeq
+          } else {
+            attribute.getStringValues.asScala.map { value =>
+              Text(value)
+            }.toSeq
+          }
+          (attribute.getName, values)
+        }.toMap
+
+        Entry(Option(e.getDn), attributes)
+      }
+  }
+
 }
