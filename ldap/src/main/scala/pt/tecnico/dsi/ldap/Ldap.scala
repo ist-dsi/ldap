f3ac32f1728d6fe19c009ca43ec540ebe7911025
David Duarte <david.duarte@tecnico.ulisboa.pt>
2016-04-07 16:52:12 +0100

    Specialise 'search' into two different methods
    This is useful if you want to get all the results or the first one only.

--- ldap/src/main/scala/pt/tecnico/dsi/ldap/Ldap.scala
+++ ldap/src/main/scala/pt/tecnico/dsi/ldap/Ldap.scala
@@ -1,9 +1,11 @@
 package pt.tecnico.dsi.ldap
 
+
 import com.typesafe.scalalogging.LazyLogging
 import org.ldaptive.pool._
 import org.ldaptive._
 
+import scala.collection.JavaConverters._
 import scala.concurrent.Future
 
 class Ldap(val settings: Settings = new Settings()) extends LazyLogging {
@@ -40,7 +42,7 @@ class Ldap(val settings: Settings = new Settings()) extends LazyLogging {
     }
   }
 
-  def search(ou: String, filter: String, attributes: String*): Future[SearchResult] = withConnection { connection =>
+  private def createSearchResult(ou: String, filter: String, attributes: Seq[String])(implicit connection: Connection) = {
     val request = new SearchRequest(s"$ou,$baseDomain", filter, attributes: _*)
     request.setDerefAliases(DerefAliases.valueOf(searchDereferenceAlias))
     request.setSearchScope(SearchScope.valueOf(searchScope))
@@ -50,8 +52,19 @@ class Ldap(val settings: Settings = new Settings()) extends LazyLogging {
     new SearchOperation(connection).execute(request).getResult
   }
 
+  def search(ou: String, filter: String, attributes: Seq[String] = Seq.empty): Future[Option[Entry]] =
+    withConnection { connection =>
+      val result: LdapEntry = createSearchResult(ou, filter, attributes).getEntry
+      fixLdapEntry(result)
+    }
+
+  def searchAll(ou: String, filter: String, attributes: Seq[String] = Seq.empty): Future[Seq[Entry]] =
+    withConnection { connection =>
+      val results: Iterable[LdapEntry] = createSearchResult(ou, filter, attributes).getEntries.asScala
+      results.toSeq.flatMap(fixLdapEntry)
+    }
+
   private def fixLdapEntry(entry: LdapEntry): Option[Entry] = {
-    import scala.collection.JavaConverters._
 
     Option(entry)
       .filter(_.getAttributes != null)
