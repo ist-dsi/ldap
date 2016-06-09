7679d01246e8141f2560351779ee6c8599bbda37
David Duarte <david.duarte@tecnico.ulisboa.pt>
2016-06-09 15:04:18 +0100

    Improve documentation of MathsProvider

--- src/main/scala/pt/tecnico/dsi/ldap/security/provider/MathsProvider.scala
+++ src/main/scala/pt/tecnico/dsi/ldap/security/provider/MathsProvider.scala
@@ -5,18 +5,20 @@ import java.security.{PrivilegedAction, AccessController, Provider}
 /**
   * A provider that for AES128CounterRNGFast, a cryptographically secure random number generator through SecureRandom
   */
-object MathsProvider extends Provider("UncommonsMath", 1.0, "uncommons.math provider 1.0 that implements a secure AES random number " +
-  "generator, for uncommons.math based on Akka Implementation") {
-  AccessController.doPrivileged(new PrivilegedAction[this.type] {
-    def run = {
-      //SecureRandom
+object MathsProvider extends Provider("UncommonsMath", 1.0, "uncommons.math provider 1.0 that implements a secure AES random number generator, for " +
+  "uncommons.math based on Akka Implementation") {
+  AccessController.doPrivileged(new PrivilegedAction[MathsProvider.type] {
+    def run: MathsProvider.type = {
+      // SecureRandom
       put("SecureRandom.AES128CounterSecureRNG", classOf[AES128CounterSecureRNG].getName)
       put("SecureRandom.AES256CounterSecureRNG", classOf[AES256CounterSecureRNG].getName)
 
-      //Implementation type: software or hardware
+      // Implementation type: software or hardware
       put("SecureRandom.AES128CounterSecureRNG ImplementedIn", "Software")
       put("SecureRandom.AES256CounterSecureRNG ImplementedIn", "Software")
-      null //magic null is magic
+
+      // As we don't want to return a value, we must return null. See https://docs.oracle.com/javase/8/docs/api/java/security/AccessController.html
+      null // scalastyle:ignore null
     }
   })
 }
